81be55df0c44624260590456586fe574a45187f2
Andrés Anaya <alvaroandres.anayaamariles@telefonica.com>
2023-10-03 09:34:15 +0200

    cloud9 terraform

---
+++ rke2/tf-rke2/cloud9.tf
@@ -0,0 +1,14 @@
+resource "aws_cloud9_environment_ec2" "bastion" {
+    name = "bastion-telefonica"
+    description = "bastion to telefonica"
+    instance_type = "t3.medium"
+    image_id = "ubuntu-18.04-x86_64"
+    subnet_id = module.vpc.private_subnets[0]
+    connection_type = "CONNECT_SSM"
+    tags = var.tags   
+    
+#    timeouts {
+#     create = "60m"
+#     delete = "2h"
+#    }
+}
