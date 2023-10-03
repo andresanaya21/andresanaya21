625182c7c789a1e75760f39e33e4ca3803eee635
Andrés Anaya <alvaroandres.anayaamariles@telefonica.com>
2023-10-03 17:08:51 +0200

    adding 9345 port listeners to load balancer terraform

--- rke2/tf-rke2/load-balancer.tf
+++ rke2/tf-rke2/load-balancer.tf
@@ -12,6 +12,14 @@ resource "aws_security_group" "nlb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow inbound traffic from anywhere on port 9345
+  ingress {
+    protocol    = "tcp"
+    from_port   = 9345
+    to_port     = 9345
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # Allow outbound traffic to anywhere on any port 
   egress {
     protocol    = "-1"
@@ -41,11 +49,28 @@ resource "aws_lb_target_group" "nlb_tg" {
   target_type = "instance"
 
 }
+resource "aws_lb_target_group" "nlb_tg_server" {
+  name     = "nlb-tg-tf-server"
+  port     = 9345
+  protocol = "TCP"
+  vpc_id   = module.vpc.vpc_id
+
+  # Use instance ID as target type
+  target_type = "instance"
+
+}
 
 # Attach the EC2 instances to the target group
 resource "aws_lb_target_group_attachment" "nlb_tg_attachment" {
   count            = length(local.list_ec2)
   target_group_arn = aws_lb_target_group.nlb_tg.arn
+  target_id        = element(local.list_ec2, count.index)
+}
+
+# Attach the EC2 instances to the target group
+resource "aws_lb_target_group_attachment" "nlb_tg_attachment_server" {
+  count            = length(local.list_ec2)
+  target_group_arn = aws_lb_target_group.nlb_tg_server.arn
   target_id        = element(local.list_ec2, count.index)
 }
 
@@ -58,5 +83,17 @@ resource "aws_lb_listener" "nlb_listener" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.nlb_tg.arn
+  }
+}
+
+
+resource "aws_lb_listener" "nlb_listener_server" {
+  load_balancer_arn = aws_lb.nlb.arn
+  port              = 9345
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nlb_tg_server.arn
   }
 }
